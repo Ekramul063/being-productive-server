1c9d6c1d1cdcafdad4f74a1c0763a4716a34a886
unknown <ekaramulhasan063@gmail.com>
2022-10-27 21:51:30 +0600

    server updated

--- index.js
+++ index.js
@@ -3,14 +3,17 @@ const app = express();
 var cors = require('cors')
 const port = process.env.PORT || 5000;
 const courses = require('./data/courses.json');
+//add cors
 app.use(cors())
-
+//test server
 app.get('/',(req,res)=>{
     res.send('test server')
 })
+//all course
 app.get('/courses',(req,res)=>{
     res.send(courses)
 })
+//single course
 app.get('/course/:id',(req,res)=>{
     const id = parseInt(req.params.id);
     const selectedItem = courses.find(course => course.id === id);
